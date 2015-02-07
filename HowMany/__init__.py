d15e407dacf3158d3189c04774c966d8b26e5e6d
Harry Reeder <harry@harryreeder.co.uk>
2015-02-07 18:22:50 +0000

    Add Sentry

--- HowMany/__init__.py
+++ HowMany/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.heroku import Heroku
+from raven.contrib.flask import Sentry
 
 from HowMany.yo import Yo
 
 app = Flask(__name__)
 heroku = Heroku(app)
-yo = Yo()
+sentry = Sentry(app)
 
+yo = Yo()
 
 from HowMany import views
