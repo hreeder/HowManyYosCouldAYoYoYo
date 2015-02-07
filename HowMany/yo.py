ad092c9e8425fb9285cbb905ddb8aef82a44d27a
Harry Reeder <harry@harryreeder.co.uk>
2015-02-07 20:08:32 +0000

    Timezone Fixes

--- HowMany/yo.py
+++ HowMany/yo.py
@@ -2,7 +2,11 @@ import datetime
 from HowMany import db
 
 
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+
 class Yo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender = db.Column(db.String)
-    sent_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    sent_at = db.Column(db.DateTime, default=get_current_time())
