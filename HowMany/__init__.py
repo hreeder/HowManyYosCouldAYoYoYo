41f203e9f0174b84ae572d31870d9313199bc738
Harry Reeder <harry@harryreeder.co.uk>
2015-02-09 09:52:07 +0000

    Add redis

--- HowMany/__init__.py
+++ HowMany/__init__.py
@@ -1,15 +1,18 @@
 import os
 from flask import Flask
 from flask.ext.heroku import Heroku
+from flask_redis import Redis
 from flask.ext.sqlalchemy import SQLAlchemy
 from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['REDIS_URL'] = os.environ['REDISCLOUD_URL']
 
 db=SQLAlchemy(app)
 heroku = Heroku(app)
 sentry = Sentry(app)
+redis_store = Redis(app)
 
 key = os.environ['APP_KEY']
 
