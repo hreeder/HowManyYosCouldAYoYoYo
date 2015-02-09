9871c1ed32fdfd1d52417330ba6d33be28a2b70f
Harry Reeder <harry@harryreeder.co.uk>
2015-02-09 12:44:54 +0000

    Yeah, about those version strings etc...

--- HowMany/filters.py
+++ HowMany/filters.py
@@ -7,11 +7,11 @@ def fuzzystamp(dtobj):
     tstring = dtobj.isoformat("T") + "Z"
     return "<time class='timeago' datetime='%s'></time>" % (tstring,)
 
-@app.context_processor
-def get_app_name():
-    return dict(app_name=app.config['APP_NAME'])
-
-@app.context_processor
-def get_app_version():
-    cloud = heroku.from_key(app.config['HEROKU_API_KEY'])
-    return dict(app_version=cloud.apps[app.config['APP_NAME']].releases[-1].name)
+# @app.context_processor
+# def get_app_name():
+#     return dict(app_name=app.config['APP_NAME'])
+#
+# @app.context_processor
+# def get_app_version():
+#     cloud = heroku.from_key(app.config['HEROKU_API_KEY'])
+#     return dict(app_version=cloud.apps[app.config['APP_NAME']].releases[-1].name)
